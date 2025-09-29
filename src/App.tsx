48c79fc375e92f2966047a17066a54f219b66b79
crynce <shobhit.choudhary22@gmail.com>
2025-09-29 15:13:12 +0530

    calendar working with left and right arrouw and date selected and saved in timestamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import FormStep2 from "./components/FormStep2";
 import LoginForm from "./components/LoginForm";
+import Calendar from "./components/Calendar";
 
 function App() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  function handleDropdownToggle() {
+    setDropdownVisible((prev) => !prev);
+  }
   return (
     <>
-      <div className="app-container">
-        <LoginForm />
+      <div className="app-container ">
+        {/* <LoginForm /> */}
+        {/* <FormStep2 /> */}
+        <Calendar />
       </div>
     </>
   );
